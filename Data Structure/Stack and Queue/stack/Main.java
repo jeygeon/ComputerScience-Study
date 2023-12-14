7f6ee34a52579f81ffcfc006161599c0495d7df6
jaeygun <jaeygun@gmail.com>
2023-12-14 18:52:27 +0900

    stack 구현(search, peek, pop)

--- Data Structure/Stack and Queue/stack/Main.java
+++ Data Structure/Stack and Queue/stack/Main.java
@@ -6,11 +6,35 @@ public class Main {
     public static <E> void main(String[] args) {
 
         Stack<E> stack = new Stack<E>(3);
-        E data = stack.push((E) "1");
-        if (data != null) {
-            System.out.println("Stack 데이터 삽입 성공 : " + data);
+        int index1 = stack.search((E) "3");
+        System.out.println("Stack data [ 3 ] index is " + index1);
+        E data1 = stack.push((E) "1");
+        if (data1 != null) {
+            System.out.println("Stack data [ " + data1 + " ] push success");
         }
+        E data2 = stack.push((E) "2");
+        if (data2 != null) {
+            System.out.println("Stack data [ " + data2 + " ] push success");
+        }
+        E data3 = stack.push((E) "3");
+        if (data3 != null) {
+            System.out.println("Stack data [ " + data3 + " ] push success");
+        }
+        E data4 = stack.push((E) "4");
+        if (data4 != null) {
+            System.out.println("Stack data [ " + data4 + " ] push success");
+        }
+        int index2 = stack.search((E) "3");
+        System.out.println("Stack data [ 3 ] index is " + index2);
 
+        E peekData1 = stack.peek();
+        System.out.println("stack peek data is [ " + peekData1 + " ]");
+
+        E popData = stack.pop();
+        System.out.println("stack pop data is [ " + popData + " ]");
+
+        E peekData2 = stack.peek();
+        System.out.println("stack peek data is [ " + peekData2 + " ]");
 
     }
 
--- Data Structure/Stack and Queue/stack/Stack.java
+++ Data Structure/Stack and Queue/stack/Stack.java
@@ -1,5 +1,7 @@
 package org.study.stack;
 
+import java.util.EmptyStackException;
+
 public class Stack<E> {
 
     private E[] arr;
@@ -9,23 +11,34 @@ public class Stack<E> {
     public Stack(int size) {
         this.arr = (E[]) new Object[size];
         this.pointer = -1;
+        System.out.println("create new stack array success. array length : [ " + size + " ]");
     }
 
-    /*
-    구현 메소드
-    empty(), peek(), pop(), push(Object o), search(Object o)
+    /**
+     * isEmpty()
+     * <br/>
+     * stack이 비워져있는지 여부 체크
+     *
+     * @return
      */
     public boolean isEmpty() {
         return this.pointer == -1 ? true : false;
     }
 
+    /**
+     * isFull()
+     * <br/>
+     * stack이 가득 찼는지 여부 체크
+     *
+     * @return
+     */
     public boolean isFull() {
         return this.pointer + 1 == this.arr.length ? true : false;
     }
 
     /**
      * push()
-     *
+     * <br/>
      * stack에 데이터 추가.<br/>
      * 실패해도 예외를 발생시키지 않음.<br/>
      * 구현할 때는 실패 시 null을 반환하도록 구현.
@@ -37,6 +50,7 @@ public class Stack<E> {
 
         // stack이 가득 찼는지 체크
         if (isFull()) {
+            System.out.println("can't push data [ " + data + " ]. stack length is full");
             return null;
         }
 
@@ -48,5 +62,80 @@ public class Stack<E> {
 
         // 추가한 데이터를 반환
         return data;
+    }
+
+    /**
+     * search()
+     * <br/>
+     * Stack에서 객체를 찾아서 그 위치를 반환<br/>
+     * 실패해도 예외를 발생시키지 않음.<br/>
+     *
+     * @param data 위치를 찾을 데이터 데이터
+     * @return 못 찾으면 -1을 반환
+     */
+    public int search(E data) {
+
+        // stack이 비어있는지 확인
+        if (isEmpty()) {
+            System.out.println("can't find data [ " + data + " ]. stack length is zero");
+            return -1;
+        }
+
+        // stack의 제일 첫번째부터 for문으로 데이터 찾기
+        E target;
+        for (int i = 0; i < this.arr.length; i++) {
+            target = this.arr[i];
+            if (target.equals(data)) {
+                return i + 1;
+            }
+        }
+
+        // 못 찾으면 -1을 반환
+        return -1;
+    }
+
+    /**
+     * peek()
+     * <br/>
+     * stack의 객체를 반환<br/>
+     * stack에서 객체를 꺼내지 않음<br/>
+     *
+     * @return E data
+     * @exception EmptyStackException
+     */
+    public E peek() {
+
+        // stack이 비워져 있는지 체크
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return this.arr[this.pointer];
+    }
+
+    /**
+     * pop()
+     * <br/>
+     * stack의 객체를 반환<br/>
+     * stack에서 객체를 꺼냄
+     *
+     * @return E data
+     * @exception EmptyStackException
+     */
+    public E pop() {
+
+        // stack이 비워져 있는지 체크
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        E targetData = this.arr[this.pointer];
+
+        // 현재 포인터 객체 초기화
+        this.arr[this.pointer] = null;
+
+        // 포인터 한단계 낮춤
+        this.pointer--;
+
+        return targetData;
     }
 }
