70759af4869364d97cb4aba930d1caebfcd5f044
jaeygun <jaeygun@gmail.com>
2024-01-08 08:05:33 +0900

    Max Heap 삭제 구현중

--- Data Structure/Heap/heap/maxHeap/MaxHeap.java
+++ Data Structure/Heap/heap/maxHeap/MaxHeap.java
@@ -36,24 +36,43 @@ public class MaxHeap {
         if (parentData != 0 && data > parentData) {
             sb.append(" > 부모 노드 보다 큰 데이터가 추가 되었습니다. Heap을 재정렬합니다.");
             System.out.println(sb.toString());
-            swap(index);
+
+            int childData;
+            int parentData;
+            // 루트 노드까지 계속 돌리면서 자식 노드의 데이터가 부모 노드보다 크다면 부모노드와 자식노드 교환
+            while(index > 1 && this.heap.get(index) > this.heap.get(index / 2)) {
+                childData = this.heap.get(index);
+                parentData = this.heap.get(index / 2);
+                this.heap.set(index / 2, childData);
+                this.heap.set(index, parentData);
+                index = index / 2;
+                System.out.println("Heap 정렬 완료 : " + this.heap.toString());
+            }
         } else {
             System.out.println(sb.toString());
         }
     }
 
-    public void swap(int index) {
+    public void remove() {
 
-        int childData;
-        int parentData;
-        // 루트 노드까지 계속 돌리면서 자식 노드의 데이터가 부모 노드보다 크다면 부모노드와 자식노드 교환
-        while(index > 1 && this.heap.get(index) > this.heap.get(index / 2)) {
-            childData = this.heap.get(index);
-            parentData = this.heap.get(index / 2);
-            this.heap.set(index / 2, childData);
-            this.heap.set(index, parentData);
-            index = index / 2;
-            System.out.println("Heap 정렬 완료 : " + this.heap.toString());
+        // 삭제 할 데이터 저장
+        int removeData = this.heap.get(1);
+
+        // 가장 마지막에 추가된 노드를 루트 노드로 지정
+        int lastNodeIndex = this.heap.size() - 1;
+        this.heap.set(1, this.heap.get(lastNodeIndex));
+
+        // 가장 마지막에 추가된 노드 자리 데이터 삭제
+        this.heap.remove(lastNodeIndex);
+
+        /*
+        루트 노드 데이터 삭제 후 가장 마지막 노드 데이터를 루트 노드에 넣고 Max Heap에 맞도록 재 정렬
+        루트 노드 데이터 삭제 후 바로 그 밑 자식 노드 크기 비교해서 부모 노드로 올려보내지 않고 이렇게 하는 이유는
+        Complete Binary Tree의 구조를 지키기 위함
+         */
+        while () {
+
         }
+
     }
 }
